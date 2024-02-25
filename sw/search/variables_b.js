345907f5bebd63be87ff6526437ca44e1979c786
GHA <push@gha>
2024-02-25 17:01:44 +0000

    update ec620049bf0f814fac9003b518e5fcfe677a8919

---
+++ sw/search/variables_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['pbuf_0',['PBUF',['../structneorv32__dm__t.html#a7512fa082103c24bf9ff407c7e6f7fcd',1,'neorv32_dm_t']]],
+  ['poly_1',['POLY',['../structneorv32__crc__t.html#a9bbcab843ddba4f9772d07478947a661',1,'neorv32_crc_t']]],
+  ['ptrspibuf_2',['ptrSpiBuf',['../structt__neorv32__spi.html#a487a6510820871ff8d0da7699463f6d9',1,'t_neorv32_spi']]]
+];
---
+++ sw/search/variables_e.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['thres_0',['THRES',['../structneorv32__gptmr__t.html#a666ebcbea13dafcf1987629f3e0092ee',1,'neorv32_gptmr_t']]],
+  ['time_5fhi_1',['TIME_HI',['../structneorv32__mtime__t.html#a1ffe0be2c45f5d7aa2686abf40cd1693',1,'neorv32_mtime_t']]],
+  ['time_5flo_2',['TIME_LO',['../structneorv32__mtime__t.html#acccaa3dd87a57ebd5f7ae504771e904e',1,'neorv32_mtime_t']]],
+  ['timecmp_5fhi_3',['TIMECMP_HI',['../structneorv32__mtime__t.html#af05df268c8f699401cf752e4249ba422',1,'neorv32_mtime_t']]],
+  ['timecmp_5flo_4',['TIMECMP_LO',['../structneorv32__mtime__t.html#ab5fa56ca5c2f9d547f58d169d6da96e5',1,'neorv32_mtime_t']]],
+  ['ttype_5',['TTYPE',['../structneorv32__dma__t.html#a70c28642195765409d97b941e98f47ca',1,'neorv32_dma_t']]],
+  ['tx_5fdata_6',['TX_DATA',['../structneorv32__slink__t.html#aca8210ad7c2fa588ff56e79c7eecde49',1,'neorv32_slink_t']]],
+  ['tx_5fdata_5flast_7',['TX_DATA_LAST',['../structneorv32__slink__t.html#a52c485dd7cf19b57a77c262d26dae68a',1,'neorv32_slink_t']]]
+];
